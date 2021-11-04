6aa51beeed7cef8453eff20dc65deb7652a381d2
马玥 <mayue@eastmoney.com>
2021-11-04 14:26:07 +0800

    feat: useEffect

--- src/useEffectD.js
+++ src/useEffectD.js
@@ -1,6 +1,7 @@
 // 用 useEffect 函数来代替生命周期函数
 
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 // // class 方式实现的生命周期
 // class Effect extends Component {
@@ -38,13 +39,52 @@ function Effect() {
   // useEffect 中定义的函数的执行不会阻碍浏览器更新视图，这些函数是异步执行的。
   useEffect(() => {
     console.log(`useEffect => You clicked ${count} times.`)
-  })
+
+    return () => {
+      console.log('========')
+    }
+  }, [count]) // 想要每次count发生变化，都进行解绑，在第二个参数里传入对应的变量
 
   return (
     <div>
       <p>You clicked { count } times.</p>
       <button onClick={() => {setCount(count + 1)}}>Click Me!</button>
+      <Router>
+        <ul>
+          <li><Link to="/">首页</Link></li>
+          <li><Link to="/list/">列表</Link></li>
+        </ul>
+        <Route path="/" component={Index} exact />
+        <Route path="/list/" component={List} />
+      </Router>
     </div>
+  )
+}
+
+// useEffect 可以用 返回一个函数的形式进行解绑，实现类似 componentWillUnmount 的效果
+// useEffect 的第二个参数是一个数组，数组中可写入很多状态对应的变量，当状态值发生变化时，才进行解绑。
+          // 当传空数组 [] 时，就是当组件被销毁时进行解绑，这样能实现 componentWillUnmount
+function Index() {
+  useEffect(() => {
+    console.log('useEffect => 你来啦！这里是 Index')
+    // 可以用 返回一个函数的形式进行解绑
+    return () => {
+      console.log('俺要走了 Index')
+    }
+  }, [])
+  return (
+    <h2>Index Mayue Page</h2>
+  )
+}
+function List() {
+  useEffect(() => {
+    console.log('useEffect => 你来啦！这里是 List')
+    return () => {
+      console.log('俺走了，List')
+    }
+  }, [])
+  return (
+    <h2>List PAge</h2>
   )
 }
 export default Effect
